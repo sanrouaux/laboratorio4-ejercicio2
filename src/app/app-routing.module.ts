0a2b631468967c9eed9a364ceb5009fbeb79dea5
santiago rouaux <sanrouaux@hotmail.com>
2021-09-01 21:02:45 -0300

    Crea rutas para los nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
